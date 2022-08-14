020ef0a80b919488cbc3963e21e822253a1efa58
zhaqian <80608733+zhaqian12@users.noreply.github.com>
2022-08-14 13:15:11 +0800

    remove marco in prehandler.h

--- qmk_porting/protocol/pre_handler.h
+++ qmk_porting/protocol/pre_handler.h
@@ -9,24 +9,3 @@
 #undef FORCE_NKRO
 #endif
 #endif
-#ifdef VIA_ENABLE
-#ifndef DYNAMIC_KEYMAP_ENABLE
-#define DYNAMIC_KEYMAP_ENABLE
-#endif
-#ifndef RAW_ENABLE
-#define RAW_ENABLE
-#endif
-#endif
-#ifdef DYNAMIC_KEYMAP_ENABLE
-#ifndef EEPROM_ENABLE
-#define EEPROM_ENABLE
-#endif
-#endif
-#ifdef EEPROM_ENABLE
-#ifndef EEPROM_DRIVER
-#define EEPROM_DRIVER
-#endif
-#if (!defined(EEPROM_CUSTOM) && !defined(EEPROM_WEAR_LEVELING))
-#define EEPROM_WEAR_LEVELING
-#endif
-#endif
